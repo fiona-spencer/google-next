a40083862ba17e8e03b93cd27a4166d23543caf7
fiona-spencer <fionagspencer@gmail.com>
2024-03-24 00:11:26 -0400

    google api search function

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,7 +1,14 @@
-export default function page() {
+
+export default async function WebSearchPage({ searchParams }) {
+  const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+  );
+  const data = await response.json();
+  const results = data.items;
   return (
     <div>
-      WebSearchPage
+      {results && results.map((results) => (
+        <h1>{results.title}</h1>
+      ))}
     </div>
   )
 }
